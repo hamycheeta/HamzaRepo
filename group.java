126eeeb82228a32704c1c00bb5b19e11a9fb7fd3
Hamza Wasim <hw2481@email.vccs.edu>
2016-10-27 11:21:51 -0400

    Group Sizer

---
+++ group.java
@@ -0,0 +1,40 @@
+import javafx.scene.control.TextInputDialog;
+import javafx.stage.Stage;
+import java.util.Optional;
+import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+/**
+* @Hamza
+*octobor 20
+*hamza96@gmailq.com
+*the purpose is to develope an application to assist athletes
+*/
+
+public  class group extends Application {
+@Override
+public void start(Stage primryStage) {
+double athlete;
+double output;
+
+TextInputDialog numpeople = new TextInputDialog();
+numpeople.setContentText("Enter number of people here");                                                                                                                                                          
+Optional<String> input = numpeople.showAndWait();                                                                                                                                                                 athlete = Double.parseDouble(input.get());
+System.out.println(athlete);
+if(athlete>10){
+output = athlete/2;
+Alert text = new Alert(AlertType.CONFIRMATION);
+text.setContentText("You have a total of " + athlete + "Groupsize of " + output);
+text.showAndWait();
+}else if(athlete>=3 && athlete<=10){
+output= athlete/3;
+Alert text = new Alert(AlertType.CONFIRMATION);
+text.setContentText("You have a total of " + athlete + "Groupsize of " + output);
+text.showAndWait();
+}else{
+Alert text = new Alert(AlertType.CONFIRMATION);
+text.setContentText(" The amount of people needs to be at least 3 ");
+text.showAndWait();
+}
+}
+}
